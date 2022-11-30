6ebbb89accbfee0332e928131497592ff882e54d
Amani Mavu <amanimavu@gmail.com>
2022-11-30 11:03:07 +0000

    use find to get string indices

--- 0x00-python-hello_world/8-concat_edges.py
+++ 0x00-python-hello_world/8-concat_edges.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
 str = "Python is an interpreted, interactive, object-oriented programing\
 		language that combines remarkable power with very clear syntax"
-str = "object-oriented programming with Python"
+str = str[str.find("object"):str.find("programing")+10] + str[str.find(" ")] + str[str.find("with"):str.find("very")]  + str[: str.find(" ")]
 print(str)
