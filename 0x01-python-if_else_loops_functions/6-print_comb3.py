5d9f8de917b8436183d89e0da569f87a60367624
Amani Mavu <amanimavu@gmail.com>
2022-12-02 14:29:31 +0000

    use parentheses to wrap if statement

--- 0x01-python-if_else_loops_functions/6-print_comb3.py
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -2,8 +2,8 @@
 
 for i in range(0, 100):
     for j in range(i + 1, 100):
-        if "{:02d}".format(j)[0] == "{:02d}".format(i)[1] and \
-            "{:02d}".format(j)[1] == "{:02d}".format(i)[0]:
+        if ("{:02d}".format(j)[0] == "{:02d}".format(i)[1] and
+                "{:02d}".format(j)[1] == "{:02d}".format(i)[0]):
             if i != 89:
                 print("{:02d}, ".format(i), end="")
             else:
