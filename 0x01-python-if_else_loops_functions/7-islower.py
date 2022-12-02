92432ca7f74252730646efa9d9fe544abaf92281
Amani Mavu <amanimavu@gmail.com>
2022-12-02 13:47:13 +0000

    correct if logic and syntax

--- 0x01-python-if_else_loops_functions/7-islower.py
+++ 0x01-python-if_else_loops_functions/7-islower.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
-def isLower(c):
-    if ord(c) >= 97 or ord(c) <= 122:
+def islower(c):
+    if ord(c) >= 97 and ord(c) <= 122:
         return True
-    else
+    else:
         return False
