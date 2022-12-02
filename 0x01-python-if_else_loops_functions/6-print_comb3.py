0c0f5b796b8a0d127f7d3b2622d4d2c9d8e9f434
Amani Mavu <amanimavu@gmail.com>
2022-12-02 13:23:27 +0000

    find smallest 2 digit combination

---
+++ 0x01-python-if_else_loops_functions/6-print_comb3.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+for i in range(0, 100):
+    for j in range(i + 1, 100):
+        if "{:02d}".format(j)[0] == "{:02d}".format(i)[1] and "{:02d}".format(j)[1] == "{:02d}".format(i)[0]:
+            if i != 89:
+                print("{:02d}, ".format(i), end="")
+            else:
+                print("89")
+    else:
+        continue
