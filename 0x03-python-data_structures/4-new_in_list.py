2f75a8cfa9ade6b35f3c14e5842fa98e7dd74404
Amani Mavu <amanimavu@gmail.com>
2022-12-03 21:55:43 +0000

    return original list when index is flawed

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -3,9 +3,9 @@
 # replace an element in a list without modifying the original
 def new_in_list(my_list, idx, element):
     if idx < 0:
-        return None
+        return my_list
     if idx >= len(my_list):
-        return None
+        return my_list
     new_list = my_list[:]
     new_list[idx] = element
     return new_list
