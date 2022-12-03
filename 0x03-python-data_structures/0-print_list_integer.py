e36c3c3b562cdc52d34de158a3dfb99bcc530a36
Amani Mavu <amanimavu@gmail.com>
2022-12-03 20:18:39 +0000

    handle case when list is empty

--- 0x03-python-data_structures/0-print_list_integer.py
+++ 0x03-python-data_structures/0-print_list_integer.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 
+# function that prints numbers in a list
 def print_list_integer(my_list=[]):
     if not my_list:
-        print("")
+        return
     else:
         for i in my_list:
             print("{}".format(i))
