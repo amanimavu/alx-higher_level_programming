4fdc9a103b0dbd17c10c98f9acbad61b127f22c5
Amani Mavu <amanimavu@gmail.com>
2022-12-03 22:46:53 +0000

    print a matrix

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+# Print a matrix of integers
+def print_matrix_integer(matrix=[[]]):
+    if matrix == [[]]:
+        print("")
+    else:
+        for row in matrix:
+            for col in range(0, len(row)):
+                if col == len(row) - 1:
+                    print("{}".format(row[col]))
+                else:
+                    print("{} ".format(row[col]), end="")
