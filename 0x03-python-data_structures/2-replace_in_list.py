d5061b6bba8276c013bc2e92334bc25f66e5a702
Amani Mavu <amanimavu@gmail.com>
2022-12-03 21:29:39 +0000

    replaces the value of an index and returns new list

---
+++ 0x03-python-data_structures/2-replace_in_list.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+# Replace the element at a given index
+def replace_in_list(my_list, idx, element):
+    if idx < 0:
+        return my_list
+    if idx >= len(my_list):
+        return my_list
+    my_list[idx] = element
+    return my_list[:]
