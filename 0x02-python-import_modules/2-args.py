faf471110f3eacc22e7f2094f89d94e0163455e7
Amani Mavu <amanimavu@gmail.com>
2022-12-06 21:46:40 +0000

    access the command line arguments

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,20 @@
+#!/usr/bin/python3
+import sys
+
+
+def main():
+    argv = sys.argv
+    len_of_argv = len(argv) - 1
+    if len_of_argv == 1:
+        print("{} argument:".format(len_of_argv))
+    if len_of_argv == 0:
+        print("{} arguments:".format(len_of_argv))
+        return None
+    else:
+        print("{} arguments:".format(len_of_argv))
+    for i in range(1, len_of_argv + 1):
+        print("{}: {}".format(i, argv[i]))
+
+
+if __name__ == "__main__":
+    main()
