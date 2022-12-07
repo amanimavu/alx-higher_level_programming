df77ae7f61ee9190326bbe604f07bffee59b0371
Amani Mavu <amanimavu@gmail.com>
2022-12-07 22:05:21 +0000

    sum unique numbers in a list

---
+++ 0x04-python-more_data_structures/2-uniq_add.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+
+
+def uniq_add(my_list=[]):
+    y = 0
+    sum_of_unique_num = 0
+    if my_list:
+        for i in set(my_list):
+            sum_of_unique_num += i
+    else:
+        return
+    return sum_of_unique_num
