ddb2a03e4a8b07b68910dfc165a9494f342c04cb
Amani Mavu <amanimavu@gmail.com>
2022-12-08 16:05:50 +0000

    compress the program to be 3 lines of code

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,3 +1,3 @@
+#!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
-    multiple_list = my_list and list(map(lambda x: x * number, my_list))
-    return multiple_list
+    return my_list and list(map(lambda x: x * number, my_list))
