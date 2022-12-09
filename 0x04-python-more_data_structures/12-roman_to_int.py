8cbc975ac4bc60f8a29a367e8066aa9a4332f637
Amani Mavu <amanimavu@gmail.com>
2022-12-09 13:58:58 +0000

    consider a case when input isn't a string or is None

--- 0x04-python-more_data_structures/12-roman_to_int.py
+++ 0x04-python-more_data_structures/12-roman_to_int.py
@@ -21,7 +21,7 @@ def roman_to_int(roman_string):
     special = {"IV": 4, "IX": 9, "XL": 40, "XC": 90, "CD": 400, "CM": 900}
     numeral = {"I": 1, "V": 5, "X": 10, "L": 50, "C": 100, "D": 500, "M": 1000}
     result, i = 0, 0
-    while i < len(roman_string):
+    while roman_string == str(roman_string) and i < len(roman_string):
         if i <= len(roman_string) - 2:
             if (roman_string[i] + roman_string[i+1]) in special:
                 result += special[roman_string[i] + roman_string[i+1]]
