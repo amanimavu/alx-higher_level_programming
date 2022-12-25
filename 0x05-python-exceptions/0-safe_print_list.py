6085ff858bd9a4243d77d887d02f8043962a5d2c
Amani Mavu <amanimavu@gmail.com>
2022-12-25 15:57:26 +0000

    correct syntax and logic

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -3,11 +3,11 @@
 def safe_print_list(my_list=[], x=0):
     for i in range(0, x + 1):
         try:
-            num = my_list[i]
-        except:
+            num = int(my_list[i])
+        except (ValueError, TypeError, IndexError):
             break
         else:
             print(num, end="")
 
     print("")
-    return(i)
+    return (i)
