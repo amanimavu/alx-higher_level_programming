7319041df48ff6d219a8494e2ae75b848ada4c42
Amani Mavu <amanimavu@gmail.com>
2022-12-26 03:37:13 +0000

    add a counter to tarck integers printed

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,12 +1,16 @@
 #!/usr/bin/python3
 
 def safe_print_list(my_list=[], x=0):
+    count = 0
     for i in range(0, x + 1):
         try:
             num = int(my_list[i])
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, TypeError):
+            continue
+        except IndexError:
             break
         else:
+            count += 1
             print(num, end="")
 
     print("")
