a20350f20edba6ecfc351effe2ebb1ff949a88e1
Amani Mavu <amanimavu@gmail.com>
2022-12-26 04:19:19 +0000

    corrent the range in for loop

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -2,7 +2,7 @@
 
 def safe_print_list(my_list=[], x=0):
     count = 0
-    for i in range(0, x + 1):
+    for i in range(0, x):
         try:
             num = int(my_list[i])
         except (ValueError, TypeError):
