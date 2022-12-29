67fbc7a82aaeee6ca5cad673d7ab44ea310934e5
Amani Mavu <amanimavu@gmail.com>
2022-12-29 22:13:37 +0000

    create a function that divides element by element 2 lists

---
+++ 0x05-python-exceptions/4-list_division.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+
+
+def list_division(my_list_1, my_list_2, list_length):
+    result_list = []
+    for i in range(0, list_length):
+        try:
+            operand1 = int(my_list_1[i])
+            operand2 = int(my_list_2[i])
+            result = operand1 / operand2
+        except (ValueError, TypeError):
+            print("wrong type")
+            result = 0
+        except IndexError:
+            print("out of range")
+            result = 0
+        except ZeroDivisionError:
+            print("division by 0")
+            result = 0
+        finally:
+            result_list.append(result)
+            continue
+    return (result_list)
