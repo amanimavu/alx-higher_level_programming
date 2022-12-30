7a238d568841d2fa113147bdc5295f32a5906470
Amani Mavu <amanimavu@gmail.com>
2022-12-30 21:33:42 +0000

    initialize a private instance attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+'''Create a Square class and include a private instance attribute'''
+
+
+class Square:
+    '''Initialize instance attributes with __init__ method'''
+    def ___init__(self, size):
+        self.__size = size
