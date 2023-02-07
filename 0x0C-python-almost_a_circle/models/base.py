a0588f74b8c23fec1ee0b2afa78d7165584cd35f
Amani Mavu <amanimavu@gmail.com>
2023-02-07 10:51:40 +0300

    initial commit

---
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+"""
+module: base
+resources: class Base, which is the base class of all class
+in this project
+"""
+
+class Base:
+    """
+    The goal of this class is to manage the id attribute in
+    all the future classes of this project and to avoid code
+    duplication (by extension, bug duplication)
+    """
+    __nb_objects = 0
+
+    def __init__(self, id="None"):
+        if id is not None:
+            self.id = id
+        else:
+            __nb_objects += 1
+            self.id = __nb_objects
