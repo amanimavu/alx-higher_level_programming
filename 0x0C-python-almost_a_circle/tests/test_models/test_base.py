3771cf25da1f022080ee4168a84a22aa64561614
Amani Mavu <amanimavu@gmail.com>
2023-02-09 19:32:15 +0300

    include the test prefix to create a test

--- 0x0C-python-almost_a_circle/tests/test_models/test_base.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_base.py
@@ -1,1 +1,16 @@
 import unittest
+from models.base import Base
+
+
+class TestBase(unittest.TestCase):
+    def test_checkId(self):
+        b1 = Base()
+        self.assertEqual(b1.id, 1)
+        b2 = Base()
+        self.assertEqual(b2.id, 2)
+        b3 = Base(12)
+        self.assertEqual(b3.id, 12)
+
+
+if __name__ == "__main__":
+    unittest.main()
