5f595212709665bc50ce37322eee680cbdb0332a
Amani Mavu <amanimavu@gmail.com>
2023-02-14 18:18:10 +0300

    correct the documentation of the test file

--- 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
+++ 0x0C-python-almost_a_circle/tests/test_models/test_rectangle.py
@@ -6,13 +6,13 @@ from models.rectangle import Rectangle
 
 
 class TestRectangle(unittest.TestCase):
-    """
-    setUp method of the TestRectangle class is called
-    every time a TestCase is encountered to make sure
-    all necessary resources required to execute the
-    TestCases are available
-    """
     def setUp(self):
+        """
+        setUp method of the TestRectangle class is called
+        every time a TestCase is encountered to make sure
+        all necessary resources required to execute the
+        TestCases are available
+        """
         print("Set up")
         self.rect_1 = Rectangle(10, 22)
         self.w1 = 37
@@ -25,20 +25,20 @@ class TestRectangle(unittest.TestCase):
         self.y2 = 9
         self.rect_4 = Rectangle(3, 3)
 
-    """
-    test_width TestCase is used to test wheteher the
-    correct width size is returned
-    """
     def test_width(self):
+        """
+        test_width TestCase is used to test wheteher the
+        correct width size is returned
+        """
         self.assertEqual(self.rect_1.width, 10)
         self.assertEqual(self.rect_2.width, 21)
 
-    """
-    test_set_width is used to test if the width
-    attribute of the rectangle is correctly set.
-    It also test validation of the width property
-    """
     def test_set_width(self):
+        """
+        test_set_width is used to test if the width
+        attribute of the rectangle is correctly set.
+        It also test validation of the width property
+        """
         self.rect_1.width = self.w1
         self.assertTrue(self.rect_1.width == 37)
         with self.assertRaises(TypeError):
@@ -46,39 +46,40 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.rect_1.width = 0
 
-    """
-    test_id_allocation is used to test whether the
-    correct id is returned
-    """
     def test_id_allocation(self):
+        """
+        test_id_allocation is used to test whether the
+        correct id is returned
+        """
         self.assertEqual(self.rect_2.id, 5)
         self.assertEqual(self.rect_3.id, 7)
 
-    """
-    The test_id_allocation_no_id tests if the correct id
-    is returned when a Rectangle object is instanciated
-    with no id and if the TestCase isn't the very first
-    one. This is because the setUp method is called every
-    time a TestCase runs
-    """
     @unittest.expectedFailure
     def test_id_allocation_no_id(self):
+        """
+        The test_id_allocation_no_id tests if the correct id
+        is returned when a Rectangle object is instanciated
+        with no id and if the TestCase isn't the very first
+        one. This is because the setUp method is called every
+        time a TestCase runs
+        """
         self.assertEqual(self.rect_1.id, 1)
 
-    """
-    test_height tests whether the correct size of the
-    height is returned
-    """
     def test_height(self):
+        """
+        test_height tests whether the correct size of the
+        height is returned
+        """
         self.assertEqual(self.rect_1.height, 22)
         self.assertEqual(self.rect_2.height, 11)
 
-    """
-    test_set_height test validates the width that is
-    to be set and checks whether the heighth property
-    is correctly set
-    """
     def test_set_height(self):
+        """
+        test_set_height test validates the width that is
+        to be set and checks whether the heighth property
+        is correctly set
+        """
+
         self.rect_2.height = self.h1
         self.assertEqual(self.rect_2.height, 30)
         with self.assertRaises(TypeError):
@@ -86,20 +87,20 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.rect_2.height = -2
 
-    """
-    test_x test whether the correct x coordinate
-    postion is returned
-    """
     def test_x(self):
+        """
+        test_x test whether the correct x coordinate
+        postion is returned
+        """
         self.assertEqual(self.rect_1.x, 0)
         self.assertEqual(self.rect_3.x, 8)
 
-    """
-    test_set_x validates the x coordinate position that
-    is to be set and checks whether the width is
-    correctly set
-    """
     def test_set_x(self):
+        """
+        test_set_x validates the x coordinate position that
+        is to be set and checks whether the width is
+        correctly set
+        """
         self.rect_1.x = self.x1
         self.assertEqual(self.rect_1.x, 4)
         self.rect_3.x = self.x2
@@ -109,20 +110,20 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.rect_3.x = -4
 
-    """
-    test_y checks whether the correct y coordinate
-    position
-    """
     def test_y(self):
+        """
+        test_y checks whether the correct y coordinate
+        position
+        """
         self.assertEqual(self.rect_1.y, 0)
         self.assertEqual(self.rect_3.y, 4)
 
-    """
-    test_set_y validates the y coordinate position
-    and checks whether the y coordinate postion is
-    correctly set
-    """
     def test_set_y(self):
+        """
+        test_set_y validates the y coordinate position
+        and checks whether the y coordinate postion is
+        correctly set
+        """
         self.rect_1.y = self.y1
         self.rect_3.y = self.y2
         self.assertEqual(self.rect_1.y, 11)
@@ -132,25 +133,20 @@ class TestRectangle(unittest.TestCase):
         with self.assertRaises(ValueError):
             self.rect_3.y = -7
 
-    """
-    test_area checks whether the correct area of
-    the Rectangle object was computed
-    """
     def test_area(self):
+        """
+        test_area checks whether the correct area of
+        the Rectangle object was computed
+        """
         self.assertEqual(self.rect_1.area(), 220)
 
-    """
-    test_str checks whether the correct string
-    is returned from __str__ magic method
-    """
     def test_str(self):
+        """
+        test_str checks whether the correct string
+        is returned from __str__ magic method
+        """
         self.assertEqual(str(self.rect_2), "[Rectangle] (5) 0/0 - 21/11")
 
 
-"""
-use the unittest module to run the test file
-only if the test file is executed from the
-terminal and not executed as module
-"""
 if __name__ == "__main__":
     unittest.main()
