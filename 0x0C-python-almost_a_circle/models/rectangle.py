5c55a3ccda21f77bf288bd880e800f8b00f46edb
Amani Mavu <amanimavu@gmail.com>
2023-02-14 18:07:39 +0300

    correct the documentation of the module

--- 0x0C-python-almost_a_circle/models/rectangle.py
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -1,124 +1,125 @@
 #!/usr/bin/python3
-from .base import Base
 
 """
 module: rectangle
 resources: class Rectangle that inherits from Base class
 """
+from base import Base
 
 
 class Rectangle(Base):
-    """
-    The __init__ method is a magic method
-    that initializes instance variables every
-    time an object is created.
-    """
     def __init__(self, width, height, x=0, y=0, id=None):
+        """
+        The __init__ method is a magic method
+        that initializes instance variables every
+        time an object is created.
+        """
         super().__init__(id)
         self.width = width
         self.height = height
         self.x = x
         self.y = y
-    """
-    The width getter method returns the size
-    of the width of the rectangle.
-    """
+
     @property
     def width(self):
+        """
+        The width getter method returns the size
+        of the width of the rectangle.
+        """
         return self.__width
 
-    """
-    The corresponding setter method is used to
-    set the size of the rectangle.
-    """
     @width.setter
     def width(self, width):
+        """
+        The corresponding setter method is used to
+        set the size of the rectangle.
+        """
         if type(width) is not int:
             raise TypeError("width must be an integer")
         if width < 1:
             raise ValueError("width must be > 0")
         self.__width = width
 
-    """
-    The height getter method is used to return the size
-    of the rectangle's height to the user.
-    """
     @property
     def height(self):
+        """
+        The height getter method is used to return the size
+        of the rectangle's height to the user.
+        """
         return self.__height
 
-    """
-    While the corresponding height setter method is used
-    to the the size of the rectangle's height.
-    """
     @height.setter
     def height(self, height):
+        """
+        While the corresponding height setter method is used
+        to the the size of the rectangle's height.
+        """
         if type(height) is not int:
             raise TypeError("height must be an integer")
         if height < 1:
             raise ValueError("height must be > 0")
         self.__height = height
 
-    """
-    The x getter method is used to set the x coordinate
-    position of the rectangle.
-    """
     @property
     def x(self):
+        """
+        The x getter method is used to set the x coordinate
+        position of the rectangle.
+        """
         return self.__x
 
-    """
-    Its corresponding setter method is used to set x
-    coordinate of the rectangle
-    """
     @x.setter
     def x(self, x):
+        """
+        Its corresponding setter method is used to set x
+        coordinate of the rectangle
+        """
         if type(x) is not int:
             raise TypeError("x must be an integer")
         if x < 0:
             raise ValueError("x must be >= 0")
         self.__x = x
 
-    """
-    The y getter method return the y coordinate
-    position of the rectangle.
-    """
     @property
     def y(self):
+        """
+        The y getter method return the y coordinate
+        position of the rectangle.
+        """
         return self.__y
 
-    """
-    The corresponding setter method is used to
-    set the y coordinate position.
-    """
     @y.setter
     def y(self, y):
+        """
+        The corresponding setter method is used to
+        set the y coordinate position.
+        """
         if type(y) is not int:
             raise TypeError("y must be an integer")
         if y < 0:
             raise ValueError("y must be >= 0")
         self.__y = y
 
-    """
-    The area method is used to return the area of the
-    specified instance rectangle to the user
-    """
     def area(self):
+        """
+        The area method is used to return the area of the
+        specified instance rectangle to the user
+        """
         return self.width * self.height
 
-    """
-    The display method print a graphical representation
-    of the rectangle using #
-    """
     def display(self):
+        """
+        The display method print a graphical representation
+        of the rectangle using #
+        """
         for row in self.height:
             print(self.width * '#')
 
-    """
-    The magical __str__ method is used to return the details
-    of the rectangle in a manner that is well presented
-    """
     def __str__(self):
+        """
+        The magical __str__ method is used to return the details
+        of the rectangle in a manner that is well presented
+        """
         return ("[Rectangle] ({}) {}/{} - {}/{}".
                 format(self.id, self.x, self.y, self.width, self.height)
                 )
