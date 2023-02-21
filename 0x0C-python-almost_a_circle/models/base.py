d79bb4ea806a0b107d5a8e35d144b973f83e6a28
Amani Mavu <amanimavu@gmail.com>
2023-02-21 18:19:02 +0300

    consider a case of None value used with to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -33,6 +33,8 @@ class Base:
         This method takes a python object and converts
         it to a json string
         """
+        if type(list_dictionaries) is None:
+            return []
         return json.dumps(list_dictionaries)
 
     @classmethod
