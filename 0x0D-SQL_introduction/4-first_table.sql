13d8a788f6856d6eb21036609450e1aeea5af244
Amani Mavu <amanimavu@gmail.com>
2023-03-20 17:41:34 +0300

    create a table in the current database without failing

---
+++ 0x0D-SQL_introduction/4-first_table.sql
@@ -0,0 +1,4 @@
+-- create a table in the current database
+-- the table should contain the columns: id[INT] and name[VARCHAR(256)]
+
+CREATE TABLE IF NOT EXISTS first_table(id INT, name VARCHAR(256));
