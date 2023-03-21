26bfba54dd4361f9ea21a111231495bc2aea8767
Amani Mavu <amanimavu@gmail.com>
2023-03-21 16:18:15 +0300

    use show create table to get a description of the table

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,6 +1,4 @@
 -- print full description of table `first_table`
 -- no using DESCRIBE or EXPLAIN
 
-SELECT * FROM INFORMATION_SCHEMA.TABLES
-WHERE TABLE_SCHEMA = 'hbtn_0c_0'
-AND TABLE_NAME = 'first_table';
+SHOW CREATE TABLE first_table;
