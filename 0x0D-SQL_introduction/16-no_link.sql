9eb66cf845a7b82088ad71f18e9ddc8b3272b0c9
Amani Mavu <amanimavu@gmail.com>
2023-03-21 13:46:32 +0300

    order the records by score

--- 0x0D-SQL_introduction/16-no_link.sql
+++ 0x0D-SQL_introduction/16-no_link.sql
@@ -1,6 +1,8 @@
 -- list all records of tha table `second_table`
 -- don't list rows without a name value
 -- results should display the score and name (in this order)
+-- have the records ordered by score
 
 SELECT score, name FROM second_table
-WHERE name IS NOT NULL;
+WHERE name IS NOT NULL
+ORDER BY score DESC;
