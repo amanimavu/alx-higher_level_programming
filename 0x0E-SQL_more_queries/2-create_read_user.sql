bddc4e51cf31fe16d1d3ddad59ad6ee908abcf86
Amani Mavu <amanimavu@gmail.com>
2023-03-27 13:58:45 +0300

    Create a database and user 1. create a database 2. create a user 3. grant select privileges to user on specified database

---
+++ 0x0E-SQL_more_queries/2-create_read_user.sql
@@ -0,0 +1,9 @@
+-- create the database ==hbtn_0d_2== and the user ==user_0d_2==
+-- ==user_0d_2== should have only SELECT privilege in the hbtn_0d_2
+-- ==user_0d_2== password should be set to ==user_0d_2_pwd==
+-- if ==hbtn_0d_2== already exists, script shouldn't fail
+-- if user ==user_0d_2== already exist, script should not fail
+
+CREATE DATABASE IF NOT EXISTS hbtn_0d_2;
+CREATE USER IF NOT EXISTS 'user_0d_2'@'localhost' IDENTIFIED BY 'user_0d_2_pwd';
+GRANT SELECT ON hbtn_0d_2.* TO 'user_0d_2'@'localhost';
