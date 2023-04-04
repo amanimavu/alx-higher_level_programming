be81e5b095214ec53b8d3d78118cd66bb1b1b54b
Amani Mavu <amanimavu@gmail.com>
2023-04-05 00:50:59 +0300

    create a database and table specify the table structure

--- 0x0E-SQL_more_queries/6-states.sql
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -4,6 +4,7 @@
 -- if table states already exists, script shou;dn't fail
 
 CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+   USE hbtn_0d_usa;
 CREATE TABLE IF NOT EXISTS states(
-    id INT,
-  name VARCHAR(256) NOT NULL)
+    id INT PRIMARY KEY NOT NULL AUTO_INCREMENT,
+  name VARCHAR(256) NOT NULL);
