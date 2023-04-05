4d135924b16a3263a90ee767d5fc1c0df42260c3
Amani Mavu <amanimavu@gmail.com>
2023-04-05 13:33:04 +0300

    display only id and name of city

--- 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
+++ 0x0E-SQL_more_queries/8-cities_of_california_subquery.sql
@@ -2,7 +2,7 @@
 -- result must be assorted in ascending order by cities.id
 -- database name will be passed as an argument of the mysql command
 
-  SELECT * FROM cities
+  SELECT id name FROM cities
    WHERE state_id = (
          SELECT id FROM states
          WHERE name = 'California')
